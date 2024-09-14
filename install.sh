f17f2ce12da44610ec00433574054641025586b9
Harshil Jani <harshil.akshaybhai@aowealth.in>
2024-09-15 01:56:56 +0530

    Installation configured by OS
    Signed-off-by: Harshil Jani <harshiljani2002@gmail.com>

--- install.sh
+++ install.sh
@@ -3,7 +3,21 @@
 # Define variables
 VERSION="v1.0.0"
 BINARY_NAME="esqlz"
-GITHUB_URL="https://github.com/Harshil-Jani/esqlz/releases/download/$VERSION/$BINARY_NAME"
+GITHUB_URL_BASE="https://github.com/Harshil-Jani/esqlz/releases/download/$VERSION"
+
+# Detect the operating system
+OS=$(uname -s)
+
+if [ "$OS" == "Darwin" ]; then
+    BINARY_NAME="${BINARY_NAME}_mac"
+elif [ "$OS" == "Linux" ]; then
+    BINARY_NAME="${BINARY_NAME}_linux"
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
+
+GITHUB_URL="$GITHUB_URL_BASE/$BINARY_NAME"
 
 # Download the binary
 echo "Downloading $BINARY_NAME..."
@@ -14,11 +28,11 @@ chmod +x "$BINARY_NAME"
 
 # Move the binary to /usr/local/bin for global usage
 echo "Installing $BINARY_NAME to /usr/local/bin..."
-sudo mv "$BINARY_NAME" /usr/local/bin/
+sudo mv "$BINARY_NAME" /usr/local/bin/esqlz
 
 # Verify the installation
-if command -v $BINARY_NAME >/dev/null 2>&1; then
-    echo "$BINARY_NAME has been installed successfully!"
+if command -v esqlz >/dev/null 2>&1; then
+    echo "esqlz has been installed successfully!"
 else
     echo "Installation failed. Please try again. If the issue persists, please open an issue at https://github.com/Harshil-Jani/esqlz/issues"
 fi
